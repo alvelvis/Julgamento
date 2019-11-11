5908db81db6ef760054518f9bdb301e45f67cafd
Elvis de Souza <elvis.desouza99@gmail.com>
2019-11-10 23:46:56 -0200

    add tweaked config.py

---
+++ config.py
@@ -0,0 +1,30 @@
+import os
+
+BASEDIR = os.path.abspath(os.path.dirname(__file__))
+APP_NAME = "julgamento"
+VALIDATE_UD = f'{os.path.abspath(os.path.dirname(__file__))}/validate.py'
+VALIDATE_LANG = 'PT'
+DEBUG = True
+SQLALCHEMY_TRACK_MODIFICATIONS = False
+EXECUTOR_PROPAGATE_EXCEPTIONS = True
+EXECUTOR_MAX_WORKERS = 1
+SECRET_KEY = 'ALSKDJHALSKDJHALK'
+SQLALCHEMY_ECHO = True
+SQLALCHEMY_COMMIT_ON_TEARDOWN = True
+OAUTHLIB_RELAX_TOKEN_SCOPE = 1
+SEND_FILE_MAX_AGE_DEFAULT = 0
+COMCORHD = False
+REPOSITORIES = [
+	'',
+]
+GOOGLE_CLIENT_ID = ''
+GOOGLE_SECRET_KEY = ''
+ALLOWED_GOOGLE_EMAILS = '''
+
+	'''.strip().replace(" ", "").replace('\t', '').replace('\n', '').split("|")
+
+UPLOAD_FOLDER = f"{os.path.abspath(os.path.dirname(__file__))}/static/uploads"
+JULGAMENTO_FOLDER = os.path.abspath(os.path.dirname(__file__))
+COMCORHD_FOLDER = f"{os.path.abspath(os.path.dirname(__file__)).rsplit('/', 1)[0]}/UD_Portuguese-Bosque/www/interrogar-ud/conllu"
+ALLOWED_EXTENSIONS = "conllu".split("|")
+SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(BASEDIR, "prod.db")
