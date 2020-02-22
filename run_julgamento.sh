a4b19f0d980a09752ef7cb6ad0deff54199ba557
Elvis de Souza <elvis.desouza99@gmail.com>
2020-02-22 14:50:23 -0300

    update

--- run_julgamento.sh
+++ run_julgamento.sh
@@ -4,6 +4,13 @@ if [ -d .git ]; then
   git pull
 fi
 
+if ! python3 -c "import tqdm"; then
+  if ! pip3 install -r requirements.txt; then
+    sudo apt install python3-pip
+    pip3 install -r requirements.txt
+  fi
+fi
+
 if [ ! -e prod.db ]; then
   python3 once.py
   sudo chmod a+rwx -R *
