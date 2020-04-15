357853c96a2ff642a65e61d9d47e939c6d4cf745
Elvis de Souza <elvis.desouza99@gmail.com>
2020-04-14 21:31:20 -0300

    minor fix

--- importar.py
+++ importar.py
@@ -359,7 +359,7 @@ def categoryAccuracy(ud1, ud2, c, coluna="DEPREL"):
                 dicionario[token.col[coluna.lower()]][0] += 1
                 if coluna == "DEPREL" and system.sentences[sentid].tokens[t].col[coluna.lower()] == token.col[coluna.lower()]:
                     dicionario[token.col[coluna.lower()]][2] += 1
-                if len(system.sentences[sentid].tokens) > t and ((coluna == "DEPREL" and system.sentences[sentid].tokens[t].col['dephead'] == token.col['dephead']) or (coluna == "UPOS")) and system.sentences[sentid].tokens[t].col[coluna.lower()] == token.col[coluna.lower()]:
+                if ((coluna == "DEPREL" and system.sentences[sentid].tokens[t].col['dephead'] == token.col['dephead']) or (coluna == "UPOS")) and system.sentences[sentid].tokens[t].col[coluna.lower()] == token.col[coluna.lower()]:
                     sys.stderr.write(coluna.lower())
                     sys.stderr.write(token.col[coluna.lower()])
                     sys.stderr.write(str(dicionario))
