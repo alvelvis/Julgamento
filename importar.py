471b4aa8feaa9487bca16ba1a75f9573c8f61b52
Elvis de Souza <elvis.desouza99@gmail.com>
2020-04-14 21:41:20 -0300

    minor fix

--- importar.py
+++ importar.py
@@ -356,8 +356,6 @@ def categoryAccuracy(ud1, ud2, c, coluna="DEPREL"):
                     dicionario[token.col[coluna.lower()]] = [0, 0, 0]
                     if not token.col[coluna.lower()] in UAS:
                         UAS[token.col[coluna.lower()]] = dict()
-                sys.stderr.write(coluna.lower())
-                sys.stderr.write(dicionario[token.col[coluna.lower()]])
                 dicionario[token.col[coluna.lower()]][0] += 1
                 if coluna == "DEPREL" and system.sentences[sentid].tokens[t].col[coluna.lower()] == token.col[coluna.lower()]:
                     dicionario[token.col[coluna.lower()]][2] += 1
